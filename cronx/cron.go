fe9ba55297fba5b3f2c573da940c970a8c0d2bcd
owenqing <825515396@qq.com>
2021-04-20 21:41:21 +0800

    修复了一些问题

--- cronx/cron.go
+++ cronx/cron.go
@@ -22,11 +22,28 @@ type CronTasks struct {
 	tasks map[string]*cronitem
 }
 
-type CronTasksInterface interface {
-	// 新增定时任务
-	RegisteTask(taskkey, cronexpr string, f func())
-	// 启动
-	RunAll()
-	// 停止
-	Stop(taskkey string)
+// 创建定时任务
+func NewCronTasks() *CronTasks {
+	cronSync.Do(func() {
+		cronTaskInstance = &CronTasks{tasks: make(map[string]*cronitem)}
+	})
+	return cronTaskInstance
+}
+
+func (c *CronTasks) RegisteTask(taskkey, cronexpr string, f func()) {
+	cronitem := &cronitem{expr: cronexpr, fn: f, cronjob: cron.New()}
+	cronitem.cronjob.AddFunc(cronitem.expr, cronitem.fn)
+	c.tasks[taskkey] = cronitem
+}
+
+// 启动所有定时任务
+func (c *CronTasks) RunAll() {
+	for _, cronx := range c.tasks {
+		cronx.cronjob.Start()
+	}
+}
+
+// 定制定时任务 by Key
+func (c *CronTasks) Stop(taskkey string) {
+	c.tasks[taskkey].cronjob.Stop()
 }
